95e47ae2b9a7489d59b0e33a2ff1fed6ac45911c
Anirudh Sharma <anirudh03sharma@gmail.com>
2019-09-03 20:56:07 +0530

    Added code for Replication Monitoring Scheduler

---
+++ core/src/main/java/org/redquark/aem/monitoring/configs/ReplicationQueueMonitoringConfig.java
@@ -0,0 +1,51 @@
+package org.redquark.aem.monitoring.configs;
+
+import org.osgi.service.metatype.annotations.AttributeDefinition;
+import org.osgi.service.metatype.annotations.AttributeType;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
+
+/**
+ * @author Anirudh Sharma
+ *
+ */
+@ObjectClassDefinition(
+		name = "Replication Queue Monitoring", 
+		description = "This configuration is used to monitor the replication queue"
+	)
+public @interface ReplicationQueueMonitoringConfig {
+	
+	@AttributeDefinition(
+			name = "Scheduler Name",
+			description = "Name of the scheduler",
+			type = AttributeType.STRING
+		)
+	public String getSchedulerName();
+	
+	@AttributeDefinition(
+			name = "Agent Name", 
+			description = "Name of the replication agent which you wish to monitor", 
+			type = AttributeType.STRING
+		)
+	public String getAgentName();
+	
+	@AttributeDefinition(
+			name = "Clear Queue",
+			description = "If true, then the queue will be cleared if stuck",
+			type = AttributeType.BOOLEAN
+		)
+	public boolean isClearQueue();
+	
+	@AttributeDefinition(
+			name = "Cron Expression",
+			description = "Cron expression to determine when this scheduler will run",
+			type = AttributeType.STRING
+		)
+	public String getCronExpression() default "0 * * * * ?";
+	
+	@AttributeDefinition(
+			name = "Enabled",
+			description = "If true, the scheduler will be enabled",
+			type = AttributeType.BOOLEAN
+		)
+	public boolean isEnabled();
+}
---
+++ core/src/main/java/org/redquark/aem/monitoring/schedulers/ReplicationQueueMonitor.java
@@ -0,0 +1,158 @@
+package org.redquark.aem.monitoring.schedulers;
+
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.time.LocalDateTime;
+import java.util.Iterator;
+import java.util.Set;
+
+import javax.management.AttributeNotFoundException;
+import javax.management.InstanceNotFoundException;
+import javax.management.MBeanException;
+import javax.management.MBeanServer;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+import javax.management.ReflectionException;
+
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.sling.commons.scheduler.ScheduleOptions;
+import org.apache.sling.commons.scheduler.Scheduler;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.metatype.annotations.Designate;
+import org.redquark.aem.monitoring.configs.ReplicationQueueMonitoringConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.day.cq.mailer.MessageGatewayService;
+
+/**
+ * @author Anirudh Sharma
+ *
+ */
+@Component(immediate = true, service = ReplicationQueueMonitor.class)
+@Designate(ocd = ReplicationQueueMonitoringConfig.class, factory = true)
+public class ReplicationQueueMonitor implements Runnable {
+
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+	private String agentName;
+	private boolean clearQueue;
+
+	@Reference
+	private Scheduler scheduler;
+
+	@Reference
+	private MessageGatewayService messageGatewayService;
+
+	@Activate
+	protected void activate(ReplicationQueueMonitoringConfig config) {
+		agentName = config.getAgentName();
+		clearQueue = config.isClearQueue();
+	}
+
+	@Modified
+	protected void modified(ReplicationQueueMonitoringConfig config) {
+
+		// Removing the scheduler
+		removeScheduler(config);
+
+		// Adding the scheduler
+		addScheduler(config);
+	}
+
+	private void addScheduler(ReplicationQueueMonitoringConfig config) {
+		if (config.isEnabled()) {
+			ScheduleOptions options = scheduler.EXPR(config.getCronExpression());
+			options.name(config.getSchedulerName());
+			options.canRunConcurrently(false);
+			scheduler.schedule(this, options);
+			log.info("Scheduler is added: {}", config.getSchedulerName());
+		} else {
+			log.info("Scheduler is disabled: {}", config.getSchedulerName());
+			removeScheduler(config);
+		}
+	}
+
+	@Deactivate
+	protected void deactivate(ReplicationQueueMonitoringConfig config) {
+		log.info("Scheduler is removed: {}", config.getSchedulerName());
+	}
+
+	private void removeScheduler(ReplicationQueueMonitoringConfig config) {
+		log.info("Removing scheduler: {}", config.getSchedulerName());
+		scheduler.unschedule(config.getSchedulerName());
+	}
+
+	@Override
+	public void run() {
+		log.info("Starting monitoring");
+		try {
+			MBeanServer server = ManagementFactory.getPlatformMBeanServer();
+			Set<ObjectName> names;
+			names = server.queryNames(new ObjectName("com.adobe.granite.replication:type=agent,*"), null);
+			Iterator<ObjectName> replicationMBean = names.iterator();
+
+			while (replicationMBean.hasNext()) {
+				ObjectName replicationAgent = replicationMBean.next();
+				if (agentName.equalsIgnoreCase((String) server.getAttribute(replicationAgent, "Id"))) {
+					boolean valid = (boolean) server.getAttribute(replicationAgent, "Valid");
+					boolean enabled = (boolean) server.getAttribute(replicationAgent, "Enabled");
+					boolean queuePaused = (boolean) server.getAttribute(replicationAgent, "QueuePaused");
+					boolean queueBlocked = (boolean) server.getAttribute(replicationAgent, "QueueBlocked");
+
+					boolean precondition = valid && enabled && !queuePaused && !queueBlocked;
+
+					if (precondition) {
+						log.info("Everything is working FINE. Queue last checked: {}", LocalDateTime.now());
+					} else {
+						log.error("Queue is NOT able to process items. Queue last checked: {}", LocalDateTime.now());
+						sendMessageToSlack();
+						if (clearQueue) {
+							log.info("Clearing queue automatically");
+							server.invoke(replicationAgent, "queueClear", new Object[] { null },
+									new String[] { String.class.getName() });
+							if ((Integer) server.getAttribute(replicationAgent, "QueueNumEntries") == 0) {
+								log.info("Queue cleared successfully");
+							} else {
+								log.error("Could not clear queue successfully");
+							}
+
+						}
+					}
+				}
+			}
+
+		} catch (MalformedObjectNameException | InstanceNotFoundException | AttributeNotFoundException
+				| ReflectionException | MBeanException e) {
+			log.error(e.getMessage(), e);
+		}
+	}
+
+	private void sendMessageToSlack() {
+
+		CloseableHttpClient client = HttpClients.createDefault();
+		HttpPost httpPost = new HttpPost(
+				"<your-slack-webhook-url>");
+
+		try {
+			StringEntity entity = new StringEntity("{\"channel\":\"#general\",\"text\":\"something from me!\"}");
+			httpPost.setEntity(entity);
+			httpPost.setHeader("Accept", "application/json");
+			httpPost.setHeader("Content-type", "application/json");
+
+			client.execute(httpPost);
+			client.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
